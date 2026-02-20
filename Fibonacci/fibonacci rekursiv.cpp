6d81616bc83baf3961b4ad5bcf38118e32977ebc
HaiPH <HaiPH@PHAMHOANGHAI>
2026-02-21 00:36:07 +0100

    done

---
+++ Fibonacci/fibonacci rekursiv.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int a)
+{
+    if (a == 0)
+    {
+        return 0;
+    }
+    else if (a == 1)
+    {
+        return 1; 
+    }
+    else 
+    {
+        return fibonacci(a-1) + fibonacci(a-2); 
+    }
+}
+
+int main()
+{
+    int n = 25; 
+    for(int i = 0; i < n + 1; i++)
+    {
+        cout << fibonacci(i) << " ";
+    }
+    return 0;
+}
