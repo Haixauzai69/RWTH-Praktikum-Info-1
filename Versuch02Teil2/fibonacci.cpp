0a8510abf75a5f7a1680528b5acd3aa97dcc6d52
HaiPH <HaiPH@PHAMHOANGHAI>
2026-03-31 16:34:56 +0200

    versuch2teil2

---
+++ Versuch02Teil2/fibonacci.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 MMXXV
+// Versuch 02
+//
+// Datei:  main.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+
+unsigned long long fibonacci(int n)
+{
+    unsigned long long previousFib = 0;
+    unsigned long long currentFib = 1;
+    if(n == 0)
+    {
+        return 0;
+    }
+    if(n == 1)
+    {
+        return 1;
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        unsigned long long newFib = previousFib + currentFib ;
+        previousFib = currentFib; 
+        currentFib = newFib; 
+    }
+    return currentFib ;
+}
+
+int main ()
+{
+    std::cout << " Fibonacci - Zahlen von 0 bis 12:" << std::endl ;
+    for (int i = 0; i <= 12 ; i ++)
+    {
+        std::cout << "f(" << i << ") = " << fibonacci(i) << std::endl ;
+    }
+    return 0;
+}
