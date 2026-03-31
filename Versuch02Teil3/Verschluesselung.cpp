4bfe459f6dbbb55e16e96a7a8157f06322aec1a8
HaiPH <HaiPH@PHAMHOANGHAI>
2026-03-31 16:35:55 +0200

    versuch2teil3

---
+++ Versuch02Teil3/Verschluesselung.cpp
@@ -0,0 +1,87 @@
+///////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 
+// 
+// Versuch 2.3: Felder
+//
+// Datei:  Verschuesselung.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+string verschluesseln(char schluessel[2][26], string wort)
+{
+	string result; 	
+	for (char c : wort)
+	{
+        // Nur Buchstaben von A bis Z verarbeiten
+        if (c >= 'A' && c <= 'Z')
+        {
+            // Index in der ersten Zeile finden
+            for (int i = 0; i < 26; i++)
+            {
+                if (schluessel[0][i] == c)
+                {
+                    result += schluessel[1][i];
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // Unveränderte Zeichen (z.B. Leerzeichen, Sonderzeichen)
+            result += c;
+        }
+    }
+
+	return result;
+}
+
+string entschluesseln(char schluessel[2][26], string wort)
+{
+	string result;
+	 for (char c : wort)
+	 {
+        // Nur Großbuchstaben A–Z verarbeiten
+        if (c >= 'A' && c <= 'Z')
+        {
+            // Index in der zweiten Zeile finden und dem Ursprungsbuchstaben zuordnen
+             for (int i = 0; i < 26; i++)
+             {
+                if (schluessel[1][i] == c)
+                {
+                    result += schluessel[0][i];
+                    break;
+                }
+            }
+           
+        } else {
+            // Unveränderte Zeichen (z.B. Leerzeichen, Satzzeichen)
+            result += c;
+        }
+    }
+	return result;
+}
+
+int main()
+{
+	char secret[2][26] = {{'A','B','C','D','E','F','G','H','I','J',
+		'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+	{'Q','W','E','R','T','Y','U','I','O','P','A','S','D',
+		'F','G','H','J','K','L','Z','X','C','V','B','N','M'}};
+
+	
+	string word;
+	cout << "Type in a text written in caplocks: " << endl;
+	getline(cin,word);
+
+	string newname = verschluesseln(secret,word); 
+	cout << newname << endl;
+	string oldname = entschluesseln(secret,newname);
+	cout << oldname << endl;
+
+	return 0;
+}
