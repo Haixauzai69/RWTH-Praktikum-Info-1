3266072c4d5559f0e3125db172e8b487b25137f5
HaiPH <HaiPH@PHAMHOANGHAI>
2026-04-03 22:54:41 +0200

    versuch 4 done

---
+++ Versuch04/Vektor.cpp
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 
+// 
+// Versuch 04: Einf�hrung Klasse
+//
+// Datei:  Vektor.cpp
+// Inhalt: Sourcedatei der Klasse Vektor
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Vektor.h"
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+Vektor::Vektor(double inX, double inY, double inZ) : x(inX), y(inY), z(inZ)
+{
+
+}
+
+Vektor::~Vektor()
+{
+
+}
+
+/**
+ * @brief Function printing the components of the vector 
+ */
+void Vektor::ausgabe() const
+{
+    std::cout << "X-Komponente: " << x << std::endl;
+    std::cout << "Y-Komponente: " << y << std::endl;
+    std::cout << "Z-Komponente: " << z << std::endl << std::endl;
+}
+
+double Vektor::laenge() const
+{
+	return sqrt(x*x + y*y + z*z);
+}
+
+Vektor Vektor::sub(const Vektor& input) const
+{
+	Vektor ergebnis = Vektor(x - input.x, y - input.y, z - input.z);
+	return ergebnis;
+}
+
+double Vektor::skalarProd(const Vektor& input) const
+{
+	return x*input.x + y*input.y + z*input.z;
+}
+
+double Vektor::winkel(const Vektor& input) const
+{
+	double ans = acos((this->skalarProd(input))/(this->laenge()*input.laenge()));
+	return (ans*180)/M_PI;
+}
+
+void Vektor::rotiereUmZ(const double rad)
+{
+	double temp = x;
+	x = x*cos(rad) - y*sin(rad);
+	y = temp*sin(rad) + y*cos(rad);
+}
---
+++ Versuch04/main.cpp
@@ -0,0 +1,57 @@
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 MMXXV
+// Versuch 04: Einf�hrung Klasse
+//
+// Datei:  main.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Vektor.h"
+
+void versuch04()
+{
+	    const double menschGroeße = 0.00170; //in km
+		const double plattform = 0.5557; //in km
+		const double erdHoehe = 6371; // in km
+
+		Vektor erdRadius(0, erdHoehe, 0);
+		Vektor aussichtsPunkt(0, erdHoehe + plattform + menschGroeße, 0);
+		Vektor sicht = erdRadius.sub(aussichtsPunkt);
+		int schritte = 0;
+		double gesamtWinkel = 0.0;
+		const double beta = 0.0000000001; // in rad
+
+		while(erdRadius.winkel(sicht) > 90.0)
+		{
+			aussichtsPunkt.rotiereUmZ(beta);
+			sicht = erdRadius.sub(aussichtsPunkt);
+			gesamtWinkel += beta;
+			schritte++;
+		}
+
+	   double sichtWeite = sicht.laenge();
+	   double sichtwinkel = gesamtWinkel * (180/M_PI); // größter Winkel umwandeln in Grad
+
+		std::cout << std::fixed << std::setprecision(4);
+		std::cout << "Sie können " << sichtWeite << " Km weit sehen." << std::endl;
+		std::cout << "Sie sind " << (plattform + menschGroeße) * 1000 << " Meter hoch." << std::endl;
+		std::cout << "Der Winkel beträgt " << sichtwinkel << " Grad" << std::endl;
+		std::cout << "Anzahl Schritte: " << schritte << std::endl;
+}
+
+int main()
+{
+    
+//    Vektor vector1(1, 0, 0);
+//    Vektor vector2(0, 1, 0);
+//
+//    vector1.ausgabe();
+//    vector2.ausgabe();
+//
+//    Vektor erdRadius(0, 0, 0);      // bitte sinnvoll initialisieren
+//    Vektor aussichtsPunkt(0, 0, 0); // bitte sinnvoll initialisieren
+//    Vektor sicht(0, 0, 0);
+	versuch04();
+
+    return 0;
+}
